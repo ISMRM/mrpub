fff8b282add8a48e069dd1770fcb807e413b841e
Agah Karakuzu <agahkarakuzu@gmail.com>
2021-04-30 15:27:21 -0400

    whatever

--- autogen_articles.py
+++ autogen_articles.py
@@ -1,6 +1,6 @@
 import requests
 import yaml
-from os import walk
+from os import walk, listdir
 
 def get_yml(inp):
     with open(inp) as file:
@@ -10,17 +10,22 @@ def get_yml(inp):
             pub[item] = doc
     return pub
 
-_, _, yaml_user = next(walk('data/research_edit'))
-_, _, yaml_exists = next(walk('data/research_autogen'))
-yaml_user = [i for i in yaml_user if i.endswith('.yml')]
-yaml_exists = [i for i in yaml_exists if i.endswith('.yml')]
+def get_list(path):
+    out = []
+    for file in listdir(path):
+        if file.endswith(".yml"):
+            out.append(file)
+    return out
+
+yaml_user = get_list('data/research_edit')
+yaml_exists = get_list('data/research_autogen')
 to_proc = list(set(yaml_user) - set(yaml_exists))
 
 if not(to_proc):
-    print('Already up-to-date, nothing to process.')
+    print('MRpub >>>>> Already up-to-date, nothing to process.')
 
 for cur_yml in to_proc:
-    print('Creating website data for: ' + cur_yml)
+    print('MRpub >>>>> Creating website data for: ' + cur_yml)
     pub = get_yml('data/research_edit/' + cur_yml)
     response = requests.get('https://api.semanticscholar.org/v1/paper/' + pub['doi'])
     if response.status_code == 200:
@@ -31,4 +36,4 @@ for cur_yml in to_proc:
         with open('data/research_autogen/' + cur_yml, 'w') as outfile:
             yaml.dump(pub, outfile, default_flow_style=False)
     else: 
-        print('Cannot find semanticscholar entry for: ' + pub['doi'] + cur_yml) 
+        print('MRpub >>>>> Cannot find semanticscholar entry for: ' + pub['doi'] + cur_yml) 
